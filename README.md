79bfa7b4f3959ea1d7c48c8b92dae3a728ccedba
ariscybertech <ariscybertech@gmail.com>
2021-12-27 08:22:43 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# flutterapp
+ 
+ Flutter Website example
+AnionCode new site
+
+ <img src="https://i.ytimg.com/vi/afdpi5IfC5A/maxresdefault.jpg"/>
---
+++ android/app/src/main/kotlin/com/example/aris_anioncodeio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aris_anioncodeio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
